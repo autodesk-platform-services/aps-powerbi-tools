2101230ac4a5429b190fe102e7e228e6dbe55d02
Petr Broz <petr.broz@autodesk.com>
2024-05-03 10:58:02 +0200

    Moved to official APS SDK.

--- services/aps-shares-app/routes/auth.js
+++ services/aps-shares-app/routes/auth.js
@@ -1,18 +1,23 @@
 const express = require('express');
-const { AuthClientThreeLegged, UserProfileApi } = require('forge-apis');
+const { SdkManagerBuilder } = require('@aps_sdk/autodesk-sdkmanager');
+const { AuthenticationClient, ResponseType, Scopes } = require('@aps_sdk/authentication');
 const { APS_CLIENT_ID, APS_CLIENT_SECRET, APS_CALLBACK_URL } = require('../config.js');
 
-let auth = new AuthClientThreeLegged(APS_CLIENT_ID, APS_CLIENT_SECRET, APS_CALLBACK_URL, ['data:read']);
-let router = express.Router();
+const sdkManager = SdkManagerBuilder.create().build();
+const authenticationClient = new AuthenticationClient(sdkManager);
+const router = express.Router();
 
 router.get('/auth/login', (req, res) => {
-    res.redirect(auth.generateAuthUrl());
+    const url = authenticationClient.authorize(APS_CLIENT_ID, ResponseType.Code, APS_CALLBACK_URL, [Scopes.DataRead]);
+    res.redirect(url);
 });
 
 router.get('/auth/callback', async (req, res, next) => {
     try {
-        const credentials = await auth.getToken(req.query.code);
-        const { body: profile } = await new UserProfileApi().getUserProfile(auth, credentials);
+        const credentials = await authenticationClient.getThreeLeggedToken(APS_CLIENT_ID, req.query.code, APS_CALLBACK_URL, {
+            clientSecret: APS_CLIENT_SECRET
+        });
+        const profile = await authenticationClient.getUserInfo(credentials.access_token);
         credentials.expires_at = Date.now() + credentials.expires_in * 1000;;
         req.session.credentials = credentials;
         req.session.user = { id: profile.sub, name: profile.name };
